5646fa2783304761209e9146bd6d1b7f0e16cfda
SprauelK <sprauel.killian@gmail.com>
2020-04-29 12:09:53 +0200

    api.php v1

--- api.php
+++ api.php
@@ -1,15 +1,20 @@
 <?php
 header('Content-Type: application/json');
 
+//depuis le site 2
+//$filieresJSON = file_get_contents("https://..url_site1..../api/getapi.php?requete=ALL&key=....");
+//json_decode puis un "tableau de tableau"
 
 if( isset($G_GET["requete"]) && !empty($_GET["requete"]) ){
 
 	if ( $_GET["requete"] == "ALL"){
-
-	echo( file_get_contents("./filieres_groupes.json") );
+		//echo( file_get_contents("./filieres_groupes.json") );
+		readfile("./filieres_groupes.json");
 	}
 }
 
+exit();
+//--------------------------------------------------------------------
 //group variable
 $group = array();
 
@@ -22,7 +27,7 @@ $etu1["prenom"]	= "prenom1";
 $etu1["email"]	= "email1";
 $etu1["filiere"]	="LPI";
 $etu1["groupe"]	= "LPI-1";
-$etu1["telephone"]	="telephone1";
+$etu1["telephone"]	= "telephone1";
 $etu1["photo"]	= "http://...";
 
 
